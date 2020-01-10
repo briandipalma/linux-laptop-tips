3af108c62314001de27069642e2e6a213343f91f
Brian Di Palma <offler@gmail.com>
2020-01-10 12:27:12 +0000

    Update find.js

--- find.js
+++ find.js
@@ -1,34 +1,52 @@
 const { spawnSync } = require("child_process");
 
+const author = "Brian";
+const ignoredBranches = ["", "master", "->", "HEAD"];
+const remote = "origin";
+const remotePrefix = new RegExp(`^${remote}\\/`);
 const spawnOptions = { encoding: "utf8" };
-const branch = spawnSync("git", ["branch", "-r"], spawnOptions);
-const branches = branch.stdout.split(/\s+/);
 
-branches.forEach(branch => {
-  if (branch === "") {
+// Options
+const showAuthorBranches = true;
+const deleteMergedBranches = false;
+
+// All remotes
+const remoteBranch = spawnSync("git", ["branch", "-r"], spawnOptions);
+const remoteBranches = remoteBranch.stdout.split(/\s+/);
+
+remoteBranches.forEach(branch => {
+  if (branch.startsWith(`${remote}/`) === false) {
+    console.log(`Skipping ${branch}, from another remote.`);
+  }
+
+  const branchName = branch.replace(remotePrefix, "");
+
+  if (ignoredBranches.includes(branchName)) {
     return;
   }
 
-  const gitShowArgs = ["show", "-s", "--format=%an %ae", branch];
-  const authorInfo = spawnSync("git", gitShowArgs, spawnOptions);
+  if (showAuthorBranches) {
+    const gitShowArgs = ["show", "-s", "--format=%an %ae", branch];
+    const authorInfo = spawnSync("git", gitShowArgs, spawnOptions);
 
-  if (authorInfo.stdout.match("Brian")) {
+    if (authorInfo.stdout.match(author)) {
+      console.log(authorInfo.stdout.trimEnd(), branchName);
+    }
+  }
+
+  if (deleteMergedBranches) {
     const gitLogArgs = ["log", "--format=oneline", `origin/master..${branch}`];
     const gitBranchInfo = spawnSync("git", gitLogArgs, spawnOptions);
     const numberOfCommitsInBranch = gitBranchInfo.stdout.split(/\n/).length - 1;
 
     if (numberOfCommitsInBranch === 0) {
-      console.log(`Deleting ${branch.replace(/origin\//, "")}`);
+      console.log(`Deleting ${branchName}`);
 
-      spawnSync("git", [
-        "push",
-        "origin",
-        // "-n",
-        "--delete",
-        branch.replace(/origin\//, "")
-      ]);
+      spawnSync(
+        "git",
+        ["push", "origin", "-n", "--delete", branchName],
+        spawnOptions
+      );
     }
-
-    // console.log(authorInfo.stdout.trimEnd(), branch, numberOfCommitsInBranch);
   }
 });
