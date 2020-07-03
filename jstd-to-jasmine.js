4f29a3e9167086b3a22f1b5c7d2c2860cacc3589
Brian Di Palma <offler@gmail.com>
2020-07-03 15:26:16 +0100

    Update jstd-to-jasmine.js

--- jstd-to-jasmine.js
+++ jstd-to-jasmine.js
@@ -6,6 +6,10 @@ const ASSERT_STRING = { callee: { name: "assertString" } };
 const ASSERT_TRUE_CALL_FILTER = { callee: { name: "assertTrue" } };
 const ASSERT_FALSE_CALL_FILTER = { callee: { name: "assertFalse" } };
 const ASSERT_EXCEPTION = { callee: { name: "assertException" } };
+const ASSERT_NULL = { callee: { name: "assertNull" } };
+const ASSERT_UNDEFINED = { callee: { name: "assertUndefined" } };
+const ASSERT_NO_EXCEPTION = { callee: { name: "assertNoException" } };
+const ASSERT_NOT_EQUALS = { callee: { name: "assertNotEquals" } };
 
 function getTestName(root, testNameArg, j) {
   // TestCase(**testName**, testCase);
@@ -32,7 +36,7 @@ function getTestName(root, testNameArg, j) {
   throw new Error("Unimplemented getTestName");
 }
 
-function getTestsVarNodes(node, root) {
+function getTestsVarNodes(node, root, testCaseExpressionStatement) {
   if (node.type === "Identifier") {
     // var **testCase** = {...}
     const testsVarDeclarator = root.findVariableDeclarators(node.name);
@@ -45,6 +49,15 @@ function getTestsVarNodes(node, root) {
     };
   }
 
+  // TestCase("Name", **{ "test ..."() {} }**)
+  if (node.type === "ObjectExpression") {
+    return {
+      variableDeclarator: node,
+      // **TestCase("Name", { "test ..."() {} })**
+      variableDeclaration: testCaseExpressionStatement,
+    };
+  }
+
   if (node.value.type === "VariableDeclarator") {
     return {
       // var **testCase** = TestCase("")
@@ -54,10 +67,20 @@ function getTestsVarNodes(node, root) {
     };
   }
 
-  throw new Error("Unimplemented getTestsDeclaration 2");
+  // `var ATest = TestCase("ATest").prototype;` path
+  if (node.value.type === "MemberExpression") {
+    return {
+      // var **ATest** = TestCase("ATest").prototype;
+      variableDeclarator: node.parent,
+      // **var** ATest = TestCase("ATest").prototype; - "VariableDeclaration"
+      variableDeclaration: node.parent.parentPath.parentPath,
+    };
+  }
+
+  throw new Error(`Unimplemented getTestsVarNodes:: ${node.type}`);
 }
 
-function getTestsDeclaration(tests, root) {
+function getTestsDeclaration(tests, root, testCaseExpressionStatement) {
   // sinon.testCase(testCase) path.
   if (tests.type === "CallExpression") {
     const callArg = tests.arguments[0];
@@ -66,12 +89,18 @@ function getTestsDeclaration(tests, root) {
     return getTestsVarNodes(callArg, root);
   }
 
-  // TestCase(testCaseName, testCase); path
-  return getTestsVarNodes(tests, root);
+  // TestCase(testCaseName, testCase); and TestCase(testCaseName, {...}); path
+  return getTestsVarNodes(tests, root, testCaseExpressionStatement);
 }
 
 function getTestFunctionData(property) {
+  // **aTest.prototype.testSomething = function() { ... }**
   if (property.type === "AssignmentExpression") {
+    // aTest[**"test Something"**] = function() { ... }
+    if (property.left.property.type === "Literal") {
+      return [property.left.property.value, property.right];
+    }
+
     return [property.left.property.name, property.right];
   }
 
@@ -85,7 +114,7 @@ function getTestFunctionData(property) {
     return [property.key.name, property.value];
   }
 
-  throw new Error("createTestExpressionStatement");
+  throw new Error("getTestFunctionData");
 }
 
 function mapJSTDFunctionName(name) {
@@ -116,8 +145,29 @@ function createTestExpressionStatement(j, property) {
   );
 }
 
+function createTestExpressionWithComments(
+  j,
+  property,
+  expressionStatementNode
+) {
+  const expressionStatement = createTestExpressionStatement(j, property);
+
+  expressionStatement.trailingComments =
+    expressionStatementNode.trailingComments;
+  expressionStatement.comments = expressionStatementNode.comments;
+
+  return expressionStatement;
+}
+
 function getJSTDTestFunctions(variableDeclarator, root, j) {
-  const variableValue = variableDeclarator.value.init;
+  let variableValue;
+
+  // TestCase("Name", **{ "test ..."() {} }**)
+  if (variableDeclarator.type === "ObjectExpression") {
+    variableValue = variableDeclarator;
+  } else {
+    variableValue = variableDeclarator.value.init;
+  }
 
   /**
    * var **aTest** = TestCase("testName"); path
@@ -138,6 +188,25 @@ function getJSTDTestFunctions(variableDeclarator, root, j) {
     return [testAssignmentExpressions.nodes(), testAssignmentExpressions];
   }
 
+  /**
+   * var **aTest** = TestCase("testName").prototype; path
+   * aTest.testSomething = function() { ... }
+   */
+  if (variableValue.type === "MemberExpression") {
+    const testCaseFilter = { name: variableDeclarator.value.id.name };
+    const identifiers = root.find(j.Identifier, testCaseFilter);
+    // Filter the declaration identifier out
+    const testIdentifiers = identifiers.filter(
+      (path) => path.value !== variableDeclarator.value.id
+    );
+    // **aTest.testSomething = function() { ... }**
+    const testAssignmentExpressions = testIdentifiers.map((path) => {
+      return path.parent.parent;
+    });
+
+    return [testAssignmentExpressions.nodes(), testAssignmentExpressions];
+  }
+
   // Else `test = { ... }` case
   return [variableValue.properties];
 }
@@ -148,9 +217,16 @@ function convertJSTDTestsToJasmine(variableDeclarator, j, testName, root) {
     root,
     j
   );
-  const testsExpressionStatements = testFunctions.map((property) =>
-    createTestExpressionStatement(j, property)
-  );
+  const testsExpressionStatements = testFunctions.map((property, index) => {
+    const nodePath = testAssignmentExpressions.at(index).get();
+    const expressionStatementNode = nodePath.parent.value;
+
+    return createTestExpressionWithComments(
+      j,
+      property,
+      expressionStatementNode
+    );
+  });
 
   return [
     j.expressionStatement(
@@ -186,16 +262,24 @@ function removeIIFE(root) {
 }
 
 function insertJasmineTests(variableDeclaration, jasmineTests) {
-  // If the test declarator is stand alone `var tests = { ... };`
-  if (variableDeclaration.value.declarations.length === 1) {
-    variableDeclaration.replace(jasmineTests);
-    // Else the declarator is sharing a declaration `var f = 10, tests = { ... }`
-  } else if (variableDeclaration.value.declarations.length > 1) {
+  // The tests can be contained in an Expression TestCase("", {...}) or can be
+  // pointed to by a variable declarator. This case covers both as long as the
+  // variable declarator is sharing a declaration `var f = 10, tests = { ... }`
+  if (
+    variableDeclaration.value.type === "ExpressionStatement" ||
+    variableDeclaration.value.declarations.length > 1
+  ) {
     const parentNode = variableDeclaration.parent.value;
     const declarationIndex = parentNode.body.indexOf(variableDeclaration.value);
 
-    // Put the Jasmine tests after the shared var declaration
+    // Put the Jasmine tests after the shared var declaration/expression
     parentNode.body.splice(declarationIndex + 1, 0, jasmineTests);
+  }
+  // If the test declarator is stand alone `var tests = { ... };`
+  else if (variableDeclaration.value.declarations.length === 1) {
+    variableDeclaration.replace(jasmineTests);
+  } else {
+    throw new Error(`insertJasmineTests: ${variableDeclaration.type}`);
   }
 }
 
@@ -212,7 +296,10 @@ function removeJSTDCode(
 
   // This only has an effect in the case the declarator is sharing a declaration
   // e.g. `var *tests = { ... }*, x = 10, ...`; if it's standalone it's replaced
-  variableDeclarator.get().prune();
+  // ObjectExpressions don't have `get` e.g. TestCase("", **{...}**)
+  if (variableDeclarator.get) {
+    variableDeclarator.get().prune();
+  }
 
   // Possible **aTest.prototype.testSomething = function() { ... }**
   if (testAssignmentExpressions) {
@@ -242,6 +329,13 @@ function findJSTDTestCaseCall(root, j) {
     return testCaseCall;
   }
 
+  // Find the JSTD `new TestCase()` call expression
+  const newtestCaseCall = root.find(j.NewExpression, TEST_CASE_CALL_FILTER);
+
+  if (newtestCaseCall.length > 0) {
+    return newtestCaseCall;
+  }
+
   // Find the JSTD `AsyncTestCase()` call expression
   const asyncTest = root.find(j.CallExpression, ASYNC_TEST_CASE_CALL_FILTER);
 
@@ -261,23 +355,27 @@ function replaceAssertions(
   filter,
   matcher,
   expected = () => [],
-  actual = standardActual
+  actual = standardActual,
+  not
 ) {
   // Find the JSTD assertion call expressions
   const assertCall = root.find(j.CallExpression, filter);
 
   if (assertCall.length > 0) {
-    assertCall.map((aC) =>
-      aC.replace(
+    assertCall.map((aC) => {
+      let expect = j.callExpression(j.identifier("expect"), actual(aC));
+
+      if (not) {
+        expect = j.memberExpression(expect, j.identifier("not"));
+      }
+
+      return aC.replace(
         j.callExpression(
-          j.memberExpression(
-            j.callExpression(j.identifier("expect"), actual(aC)),
-            j.identifier(matcher)
-          ),
+          j.memberExpression(expect, j.identifier(matcher)),
           expected(aC)
         )
-      )
-    );
+      );
+    });
   }
 }
 
@@ -299,6 +397,43 @@ function findAndReplaceJSTDAssertions(root, j) {
   // JSTD `assertException()` call expressions
   // assertException([msg], callback, error)
   replaceAssertions(root, j, ASSERT_EXCEPTION, "toThrowError", errorE, errorA);
+  // JSTD `assertNull()` call expressions
+  replaceAssertions(root, j, ASSERT_NULL, "toBeNull");
+  replaceAssertions(root, j, ASSERT_UNDEFINED, "toBeUndefined");
+  replaceAssertions(
+    root,
+    j,
+    ASSERT_NOT_EQUALS,
+    "toEqual",
+    expected,
+    undefined,
+    true
+  );
+
+  // Find the pointless JSTD assertion call expression
+  const assertNoErrorCall = root.find(j.CallExpression, ASSERT_NO_EXCEPTION);
+
+  if (assertNoErrorCall.length > 0) {
+    assertNoErrorCall.map((aCall) => {
+      const expressionStatement = aCall.parent;
+      const parentNode = expressionStatement.parent.value;
+      const statementIndex = parentNode.body.indexOf(expressionStatement.value);
+
+      let assertCallBody;
+
+      // assertNoException(***"message"***, function () { ... }
+      if (aCall.value.arguments[0].type === "Literal") {
+        assertCallBody = aCall.value.arguments[1].body.body;
+      } else {
+        assertCallBody = aCall.value.arguments[0].body.body;
+      }
+
+      // Put the assert function contents after the assert call
+      parentNode.body.splice(statementIndex + 1, 0, ...assertCallBody);
+      // Remove the assertion call (an expression statement) parent
+      expressionStatement.replace();
+    });
+  }
 }
 
 export default function transformer(file, api) {
@@ -306,8 +441,11 @@ export default function transformer(file, api) {
   const root = j(file.source);
   const testCaseCall = findJSTDTestCaseCall(root, j);
 
+  // These assertions can be used even in Jasmine tests
+  findAndReplaceJSTDAssertions(root, j);
+
   if (testCaseCall === undefined) {
-    return;
+    return root.toSource();
   }
 
   // Find the JSTD test name and tests `TestCase("name", tests)`
@@ -317,7 +455,8 @@ export default function transformer(file, api) {
   // Get tests declarator `var *tests = {}*` and declaration `*var tests = {}*`
   const { variableDeclarator, variableDeclaration } = getTestsDeclaration(
     testsArg,
-    root
+    root,
+    testCaseCall.get().parent
   );
   // Transform the JSTD tests into Jasmine (`describe`/`it`) tests
   const [jasmineTests, testAssignmentExpressions] = convertJSTDTestsToJasmine(
@@ -329,7 +468,6 @@ export default function transformer(file, api) {
 
   // Replace the JSTD tests declaration with the Jasmine ones
   insertJasmineTests(variableDeclaration, jasmineTests);
-  findAndReplaceJSTDAssertions(root, j);
   // Remove the JSTD `TestCase()` call expression
   testCaseCall.remove();
   // Remove the JSTD test name declarator `var *testName = "ATest"*` and tests
